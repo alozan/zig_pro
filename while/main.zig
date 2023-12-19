8128d359f7568bcd19dcca77de64750c359e49cc
Angel Eduardo Lozano Niño <alozan@users.noreply.github.com>
2023-12-19 09:55:25 -0600

    Update main.zig

--- while/main.zig
+++ while/main.zig
@@ -1,15 +1,42 @@
-const expect = @import("std").testing.expect;
+const std = @import("std");
+const warn = std.debug.warn;
 
 pub fn main() void {
+    simpleWhileExample();
+    continueExpressionExample();
+}
 
-	
-  	 const a = true;
-   	 var x: u16 = 0;
-   	 if (a) {
-       	 x += 1;
-   	 } else {
-       	 x += 2;
-    	}
-   	 try expect(x == 1);
-	
+pub fn simpleWhileExample() void {
+    warn("\nExample of a simple while loop to calculate the first 10 Fibonacci numbers.\n");
+    var f_nminus2: u32 = 0;
+    var f_nminus1: u32 = 1;
+    var f_n: u32 = 1;
+
+    var i: usize = 0;
+    while (i < 10) {
+        warn("{} ", f_n);
+        f_n = f_nminus1 + f_nminus2;
+        f_nminus2 = f_nminus1;
+        f_nminus1 = f_n;
+        i += 1;
+    }
+    warn("\n");
+}
+
+pub fn continueExpressionExample() void {
+    warn("\nSame example, with loop counter in a continue expression\n");
+    var f_nminus2: u32 = 0;
+    var f_nminus1: u32 = 1;
+    var f_n: u32 = 1;
+
+    var i: usize = 0;
+    while (i < 10) : ({
+        i += 1;
+    }) {
+        warn("{} ", f_n);
+        f_n = f_nminus1 + f_nminus2;
+        f_nminus2 = f_nminus1;
+        f_nminus1 = f_n;
+    }
+    warn("\n");
 }
